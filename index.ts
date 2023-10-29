53c0551269864537590c06d8c3f8e8a979dd98f1
LAIBANADEEM106 <139864626+LaibaNadeem106@users.noreply.github.com>
2023-10-29 07:18:17 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,43 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let todos: string[] = [];
+let loop = true;
+
+while(loop){
+    const answers : {
+        TODO: string,
+        addmore: boolean
+    }= await inquirer.prompt([
+        {
+            type:"input",
+            name:"TODO",
+            message:"What do you want to add in your todo"
+        },
+        {
+            type:"confrim",
+            name:"addmore",
+            message:"Do you want to add more todo?",
+            default: false
+        }
+    ])
+    const {TODO, addmore} = answers;
+    console.log(answers)
+    loop = addmore
+    if(TODO){
+        todos.push(TODO)
+
+    }else{
+        console.log("Kindly add valid input")
+    }
+}
+
+if(todos.length > 0){
+    console.log("your Todo List: \n")
+    todos.forEach(todo => {
+        console.log(todo)
+    })
+  
+}else{
+    console.log("no todos found")
+}
